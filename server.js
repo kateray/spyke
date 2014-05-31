32e770816978a8711c6c047e0b66986c9ef2c8fd
Kate Ray <kray@Kathryns-MacBook-Air.local>
2014-05-31 12:12:36 -0400

    don't require port 3000

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+var port = process.env.PORT || 3000;
+
+http.listen(port, function(){
   console.log('Listening on port 3000');
 });
